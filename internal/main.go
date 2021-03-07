1b546b2390672a0c663678d4f61710758e46a2be
Shangfeng Liu <codershangfeng@gmail.com>
2021-03-07 15:04:34 +0800

    (WIP) add two numbers.

--- internal/main.go
+++ internal/main.go
@@ -11,3 +11,77 @@ func twoSum(nums []int, target int) []int {
 
 	return []int{}
 }
+
+type ListNode struct {
+    Val int
+    Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	l := ListNode{}
+
+	add(&l, l1, l2, false)
+
+	return &l
+}
+
+func add(l *ListNode, l1 *ListNode, l2 *ListNode, offset bool) *ListNode {
+	if !offset {
+		if l1 == nil || l2 == nil {
+			return l
+		}
+		l.Val = l1.Val + l2.Val
+		
+		
+		next := ListNode{}
+		if l1.Next != nil || l2.Next != nil {
+			l.Next = &next
+		}
+		if l.Val < 10 {
+			if l1.Next == nil && l2.Next == nil {
+				return l
+			}
+			l.Next = &next
+			add(l.Next, l1.Next, l2.Next, false)
+		} else {
+			l.Val %= 10
+			add(l.Next, l1.Next, l2.Next, true)
+		}
+		return l
+	}
+	
+	if l1 == nil && l2 == nil {
+		l.Next = &ListNode{Val: 1, Next: nil}
+		return l
+	}
+	
+	next := ListNode{}
+	if l1 == nil {
+		l.Val = l2.Val + 1
+		if l.Val >= 10 {
+			l.Next = &next
+			l.Val %= 10
+			add(l.Next, nil, nil, true)
+		}
+		return l
+	}
+
+	if l2 == nil {
+		l.Val = l1.Val + 1
+		if l.Val >= 10 {
+			l.Next = &next
+			l.Val %= 10
+			add(l.Next, nil, nil, true)
+		}
+		return l
+	}
+	
+	l.Val = l1.Val + l2.Val + 1
+	if l.Val >= 10 {
+		add(l.Next, l1.Next, l2.Next, true)
+	} else {
+		add(l.Next, l1.Next, l2.Next, false)
+	}
+	return l
+}
+
--- internal/main_test.go
+++ internal/main_test.go
@@ -21,3 +21,19 @@ func TestTwoSum(t *testing.T) {
 
 	assert.DeepEqual(t, got, []int{1, 2})
 }
+
+func TestAddTwoNumbers(t *testing.T) {
+
+	// l1 := buildListNode(2, buildListNode(4, buildListNode(3, nil)))
+	l1 := buildListNode(0, nil)
+	// l2 := buildListNode(5, buildListNode(6, buildListNode(4, nil)))
+	l2 := buildListNode(0, nil)
+
+	got := addTwoNumbers(l1, l2)
+
+	assert.DeepEqual(t, got, []int{7, 0, 8})
+}
+
+func buildListNode(val int, next *ListNode) *ListNode {
+	return &ListNode{Val: val, Next: next}
+}
